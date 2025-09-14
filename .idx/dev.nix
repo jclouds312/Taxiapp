07c1590ee2f6060f7361afd034eeb2179314961f
john474nvallejo <john474nvallejo@gmail.com>
2025-09-14 17:32:03 +0000

    update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,3 +1,4 @@
+
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
@@ -6,34 +7,36 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.jdk11
+    pkgs.android-sdk
   ];
 
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    JAVA_HOME = "${pkgs.jdk11}";
+    ANDROID_HOME = "${pkgs.android-sdk}";
+    ANDROID_SDK_ROOT = "${pkgs.android-sdk}";
+  };
+  
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
 
-    # Enable previews
+    # Enable previews and customize ports
     previews = {
       enable = true;
       previews = {
         # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
+        #   # Example: run \"npm run dev\" with PORT set to IDX\'s defined port for previews,
+        #   # and show it in IDX\'s web preview panel
+        #   command = [\"npm\" \"run\" \"dev\"];
+        #   manager = \"web\";
         #   env = {
         #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
+        #     PORT = \"$PORT\";
+        #   };\
         # };
       };
     };
@@ -47,8 +50,8 @@
       };
       # Runs when the workspace is (re)started
       onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        # The first time the workspace starts, we need to accept the Android SDK licenses
+        accept-licenses = "yes | ${pkgs.android-sdk}/bin/sdkmanager --licenses";
       };
     };
   };
