8c5e50a006103fe12d56d5cf238938dd41d6f475
john474nvallejo <john474nvallejo@gmail.com>
2025-09-24 23:55:08 +0000

    Fixed all AndroidX migration issues and enabled multidex

--- app/src/main/java/in/techware/ladriver/activity/AboutActivity.java
+++ app/src/main/java/in/techware/ladriver/activity/AboutActivity.java
@@ -3,7 +3,7 @@ package in.techware.ladriver.activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
